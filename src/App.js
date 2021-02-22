735849de11223a539d4197f8f13657d8075c15f7
Kara Schmidt <kara.jane.schmidt@gmail.com>
2021-02-22 11:19:38 -0500

    removing repo name from path

--- src/App.js
+++ src/App.js
@@ -17,10 +17,10 @@ const App = () => {
       <div>
         <Navbar />
         <Switch>
-          <Route exact path="react-portfolio/" component={Home} />
-          <Route exact path="react-portfolio/about" component={About} />
-          <Route exact path="react-portfolio/projects" component={Projects} />
-          <Route exact path="react-portfolio/contact" component={Contact} />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/projects" component={Projects} />
+          <Route exact path="/contact" component={Contact} />
         </Switch>
         <Footer />
       </div>
